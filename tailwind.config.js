8c9a5f68e937bddb0cb66ee20a56a6548d01fe2c
BRTAZ-OFC <everson_dogao@hotmail.com>
2026-09-03 14:57:03 -0300

    Backup standalone player and music search

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['ui-sans-serif', 'system-ui', 'sans-serif'],
+        body: ['ui-sans-serif', 'system-ui', 'sans-serif'],
+        mono: ['ui-monospace', 'SFMono-Regular', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
