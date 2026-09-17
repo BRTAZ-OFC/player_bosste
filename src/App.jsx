95b93b05e7683a60feb456ddf8b2ad561410a598
BRTAZ-OFC <everson_dogao@hotmail.com>
2026-09-16 23:34:47 -0300

    feat: redirect root path to /player
    Update routing to move the main PlayerBuster view to the /player path and redirect the root index to maintain cleaner URL structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import ScrollToTop from './components/ScrollToTop';
 import PlayerBuster from '@/pages/PlayerBuster';
@@ -12,7 +12,8 @@ function App() {
       <Router>
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<PlayerBuster />} />
+          <Route path="/" element={<Navigate to="/player" replace />} />
+          <Route path="/player" element={<PlayerBuster />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
